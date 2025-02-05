1d3e6b65c52c722aeb41d9f261b4fdd5fa3c272f
fannina22 <40376974+viket22@users.noreply.github.com>
2025-02-05 03:41:42 -0500

    small edits

---
+++ include/person_database.h
@@ -0,0 +1,30 @@
+// person_database.h
+
+#pragma once
+#include <person.h>
+
+class PersonDatabase
+{
+private:
+	// will eventually hold an array of Person objects.
+	Person* my_array;
+	// the size of my_array array.
+	unsigned int my_array_size;
+public:
+	// this method should open the file, reading in one person per line.
+	// ex: 101 John Doe 42 13.5
+	// ex: 102 Jane Doe 48 14.5
+	std::string my_file_name = " ";
+	PersonDatabase(std::string fname);
+
+	// adds a person.
+	void add_person(Person p);
+	// removes a person.
+	bool remove_person(int id_number);
+	// returns number of people in database as int.
+	int get_num_people();
+	// returns database as a string.
+	std::string to_string();
+
+	~PersonDatabase();
+};
